b67d52c8b04cde8c0f4679010632d8f628268ff3
cfwest86 <cfwest86@gmail.com>
2021-07-16 15:00:54 -0400

    .

--- charts.js
+++ charts.js
@@ -1,9 +1,11 @@
 function init() {
+  // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
 
+  // Use the list of sample names to populate the select options
   d3.json("samples.json").then((data) => {
-    
     var sampleNames = data.names;
+
     sampleNames.forEach((sample) => {
       selector
         .append("option")
@@ -11,31 +13,47 @@ function init() {
         .property("value", sample);
     });
 
-  buildCharts(sampleNames[0])
-  buildMetadata(sampleNames[0])
-})}
-
-init();
-
-function optionChanged(newSample) {
-  buildMetadata(newSample);
-  buildCharts(newSample);
-}
-
-function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
-    var metadata = data.metadata;
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    var result = resultArray[0];
-    var PANEL = d3.select("#sample-metadata");
-
-    PANEL.html("");
-    Object.entries(result).forEach(([key, value]) => {
-      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`.bold);
-    });
+    // Use the first sample from the list to build the initial plots
+    var firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
   });
 }
 
+// Initialize the dashboard
+init();
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildMetadata(newSample);
+  buildCharts(newSample);
+  
+}
+
+// Demographics Panel 
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    // Filter the data for the object with the desired sample number
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    // Use d3 to select the panel with id of `#sample-metadata`
+    var PANEL = d3.select("#sample-metadata");
+
+    // Use `.html("") to clear any existing metadata
+    PANEL.html("");
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+
+  });
+}
+
+// 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
@@ -47,29 +65,30 @@ function buildCharts(sample) {
     var firstSample = sampleNumber[0];
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-    var otuID = firstSample.otu_ids;
-    var otuLabels = firstSample.otu_labels;
-    var sampleValues = firstSample.sample_values;
+    var otuID = firstSample.otu_ids
+    var otuLabels = firstSample.otu_labels
+    var sampleValues = firstSample.sample_values 
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = otuID.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+    var yticks = otuID.slice(0,10).reverse()
 
     // 8. Create the trace for the bar chart. 
-    var barData = [{
-      x: sampleValues.slice(0,10).reverse(),
+    var barData = {
+      x: sampleValues,
       y: yticks,
-      text: otuLabels.slice(0,10).reverse(),
-      type: "bar",
-      orientation: "h"
-    }];
+      text: otuLabels,
+      type: "bar"
+    };
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-      title: "Top 10 Bacteria Cultures Found"
+      title: "Top 10 Bacteria Cultures Found",
+      xaxis: { title: "number of samples"},
+      yaxis: {title: "bacteria"}
     };
     // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot("bar", barData, barLayout);
+    Plotly.newPlot("bar", barData, barLayout)
   });
 }
